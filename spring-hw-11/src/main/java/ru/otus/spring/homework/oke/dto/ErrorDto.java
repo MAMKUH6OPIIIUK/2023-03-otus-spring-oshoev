fc34e7882052837a17c56e93d04c409a9bccea53
Kirill Oshoev <oshoev.kirill@gmail.com>
2023-07-15 15:21:15 +0300

    Добавил обработку всех остальных исключений, кроме валидации и кастомного NotFoundException. В обработчики добавил логирование stackTrace. В возвращаемый клиенту ErrorDto добавил stackTrace

--- spring-hw-11/src/main/java/ru/otus/spring/homework/oke/dto/ErrorDto.java
+++ spring-hw-11/src/main/java/ru/otus/spring/homework/oke/dto/ErrorDto.java
@@ -1,16 +1,20 @@
 package ru.otus.spring.homework.oke.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Data
+@NoArgsConstructor
 public class ErrorDto {
     private String message;
 
     private Map<String, List<String>> fields;
+
+    private String stackTrace;
 
     public ErrorDto(String message) {
         this.message = message;
@@ -20,5 +24,10 @@ public class ErrorDto {
     public ErrorDto(String message, Map<String, List<String>> fields) {
         this.message = message;
         this.fields = fields;
+    }
+
+    public ErrorDto(String message, String stackTrace) {
+        this.message = message;
+        this.stackTrace = stackTrace;
     }
 }
